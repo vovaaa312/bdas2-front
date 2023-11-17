cbfab21a5cc39566de67fea4cd15a996201150ac
Makarenko Vladimir <makarenkovladimir57@gmail.com>
2023-11-17 14:47:34 +0100

    init

---
+++ src/main/java/com/clinicapp/repository/UzivatelRepository.java
@@ -0,0 +1,46 @@
+package com.clinicapp.repository;
+
+import com.clinicapp.model.Pacient;
+import com.clinicapp.model.Uzivatel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UzivatelRepository {
+
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public UzivatelRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(Uzivatel uzivatel) {
+        jdbcTemplate.update("INSERT INTO UZIVATELE (ID_UZIVATEL, JMENO, HESLO,ROLE,ZAMESTNANCI_ID_ZAMESTNANEC,PACIENTI_ID_PACIENT) VALUES(?,?,?,?,?,?)",
+                new Object[]{uzivatel.getId(), uzivatel.getJmeno(), uzivatel.getHeslo(), uzivatel.getRole(), uzivatel.getZamestnanecId(), uzivatel.getPacientId()});
+    }
+    public List<Uzivatel> findAll() {
+        return jdbcTemplate.query("SELECT * from UZIVATELE",
+                BeanPropertyRowMapper.newInstance(Uzivatel.class));
+    }
+
+    public List<Uzivatel> findByJmeno(String jmeno){
+        return jdbcTemplate.query("SELECT * from UZIVATELE WHERE jmeno=?",
+                BeanPropertyRowMapper.newInstance(Uzivatel.class), jmeno);
+    }
+
+    public Uzivatel findById(Integer id) {
+        try {
+            Uzivatel uzivatel = jdbcTemplate.queryForObject("SELECT * FROM UZIVATELE WHERE id=?",
+                    BeanPropertyRowMapper.newInstance(Uzivatel.class), id);
+
+            return uzivatel;
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/clinicapp/service/UzivatelService.java
@@ -0,0 +1,31 @@
+package com.clinicapp.service;
+
+import com.clinicapp.model.Uzivatel;
+import com.clinicapp.repository.UzivatelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UzivatelService {
+
+     UzivatelRepository uzivatelRepository;
+
+     @Autowired
+    public UzivatelService(UzivatelRepository uzivatelRepository) {
+        this.uzivatelRepository = uzivatelRepository;
+    }
+
+    public List<Uzivatel> findAll(){
+        return uzivatelRepository.findAll();
+    }
+
+    public Uzivatel getById(int id){
+        return uzivatelRepository.findById(id);
+    }
+
+    public void save(Uzivatel uzivatel){
+         uzivatelRepository.save(uzivatel);
+    }
+}
